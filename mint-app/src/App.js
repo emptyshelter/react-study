3c0d4cd5241b521bbd552c6b1441e3de417d15f6
Mintae Kim <growworld@naver.com>
2020-07-16 22:07:54 +0900

    'counter'

--- mint-app/src/App.js
+++ mint-app/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+let number = 0;
+
+function plus() {
+  number += 1;
+  document.getElementById("number").innerHTML = number;
+};
+
+function minus() {
+  number--;
+  document.getElementById("number").innerHTML = number;
+};
+function reset() {
+  console.log("리셋합니다");
+  number = 0;
+  document.getElementById("number").innerHTML = number;
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hellow React
-        </a>
+        <h1 id="number">{number}</h1>
+        <input type="button" value="+" onClick={plus}></input>
+        <input type="button" value="-" onClick={minus} ></input>
+        <input type="button" value="reset" onClick={reset}></input>
       </header>
     </div>
   );
 }
-
 export default App;
